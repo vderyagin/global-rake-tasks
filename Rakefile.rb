2c39238815418bdb0dfbdf74bd8e38d6be5b8a2b
Victor Deryagin <vderyagin@gmail.com>
2012-09-12 13:09:49 +0300

    added Rakefile with task for  generatin Readme with list of tasks, also added Readme itself.

---
+++ Rakefile.rb
@@ -0,0 +1,15 @@
+desc 'Generate Readme.md'
+file 'Readme.md' => 'Rakefile.rb' do
+  require 'erb'
+  readme = <<EOL
+# List of tasks: #
+
+```
+<%= `rake --system --tasks`.strip %>
+```
+EOL
+
+  File.open './Readme.md', 'w' do |file|
+    file.puts ERB.new(readme).result
+  end
+end
